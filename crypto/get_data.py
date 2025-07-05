ca3771d6be90117912aa2cf59a97619d4f98f3e8
ckish3 <ckish1@gmail.com>
2025-07-05 12:35:41 -0700

    create table enties for the price download

--- crypto/get_data.py
+++ crypto/get_data.py
@@ -1,12 +1,18 @@
 import requests
 import pandas as pd
-from collections import defaultdict
+from typing import List
+import datetime
+
+from crypto.crypto_price import CryptoPrice
 
 
-def get_price_history(start: str, end: str, key: str, asset: str='bitcoin') -> pd.DataFrame:
+def download_price_history(start: str, end: str, key: str, asset: str) -> List:
     """
     Download the price history of a crypto asset
 
+    The API response is of the form:
+        {"data":[{"priceUsd":"104863.9648002069573128","time":1748822400000,"date":"2025-06-02T00:00:00.000Z"},
+            {"priceUsd":"105766.4432982652302685","time":1748908800000,"date":"2025-06-03T00:00:00.000Z"}]}
     Args:
         start (str): The timestamp in milliseconds of the start date
         end (str): The timestamp in milliseconds of the end date
@@ -27,11 +33,34 @@ def get_price_history(start: str, end: str, key: str, asset: str='bitcoin') -> p
 
     data = data['data']
 
-    response_data = defaultdict(list)
+    return data
 
+def get_new_price_table_entries(start: str, end: str, key: str, asset: str='bitcoin') -> List[CryptoPrice]:
+    """
+        Download the price history of a crypto asset and return a list of new price table entries for them
+    Args:
+        start (str): The timestamp in milliseconds of the start date
+        end (str): The timestamp in milliseconds of the end date
+        key (str): The API key
+        asset (str): The name of the crypto asset, e.g. 'bitcoin'
+
+    Returns:
+        List[CryptoPrice]: A list of new price table entries
+    """
+
+    data = download_price_history(start, end, key, asset)
+    table_rows = []
     for e in data:
-        for k, v in e.items():
-            response_data[k].append(v)
+        date = datetime.datetime.strptime(e['date'], '%Y-%m-%dT%H:%M:%S.000Z').date().isoformat()
+        price = e['priceUsd']
+        id = asset + '_' + date
 
-    result = pd.DataFrame(response_data)
-    return result
+        row = CryptoPrice(
+            id=id,
+            currency_name=asset,
+            date=date,
+            price=price
+        )
+        table_rows.append(row)
+
+    return table_rows
